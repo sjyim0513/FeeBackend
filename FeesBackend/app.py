39bf8a8672d03669def0920f4af47718fd491435
sjyim0513 <sjyim0513@naver.com>
2023-10-20 04:16:36 +0900

    Add files via upload

---
+++ FeesBackend/app.py
@@ -0,0 +1,32 @@
+from flask import Flask, request, jsonify
+import sqlite3
+
+app = Flask(__name__)
+
+# SQLite 데이터베이스 연결
+def connect_to_database(db_name):
+    return sqlite3.connect(db_name)
+
+@app.route('/get_total_fee', methods=['GET'])
+def get_total_fee():
+    # 요청에서 db_name과 table_name을 얻습니다.
+    db_name = request.args.get('db_name')
+    table_name = request.args.get('table_name')
+    if db_name is None or table_name is None:
+        return jsonify({'error': 'Both db_name and table_name are required'}), 400
+
+    # 테이블에서 totalfee0와 totalfee1 값을 가져옵니다.
+    conn = connect_to_database(db_name)
+    cursor = conn.cursor()
+    cursor.execute(f"SELECT totalFee0, totalFee1 FROM {table_name}")
+    result = cursor.fetchone()
+    conn.close()
+
+    if result:
+        totalFee0, totalFee1 = result
+        return jsonify({'totalFee0': totalFee0, 'totalFee1': totalFee1})
+    else:
+        return jsonify({'error': 'Table not found'}), 404
+
+if __name__ == "__main__":
+    app.run()
---
+++ FeesBackend/main.py
@@ -0,0 +1,125 @@
+import sqlite3
+from web3 import Web3
+import redis
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+# Ethereum 네트워크에 연결
+w3 = Web3(Web3.HTTPProvider(os.getenv("W3_URL")))
+Scrollw3 = Web3(Web3.HTTPProvider(os.getenv("SEP_W3_URL")))
+WEEK = 600
+pairFactory_address = "0x72A4B2F0EC0D588D6c3388b294D5c4EE8af9Bc95"
+minter_address = "0x886cAE8Ac5DD9cB375CF27B58e48B583B5b121b5"
+pair_abi = [
+    {
+        "anonymous": False,
+        "inputs": [
+            {"indexed": True, "internalType": "address", "name": "sender", "type": "address"},
+            {"indexed": False, "internalType": "uint256", "name": "amount0", "type": "uint256"},
+            {"indexed": False, "internalType": "uint256", "name": "amount1", "type": "uint256"}
+        ],
+        "name": "Fees",
+        "type": "event"
+    }
+]
+minter_abi = [
+    {
+        "inputs": [],
+        "name": "active_period",
+        "outputs": [
+            {
+                "internalType": "uint256",
+                "name": "",
+                "type": "uint256",
+            },
+        ],
+        "stateMutability": "view",
+        "type": "function",
+    },
+]
+
+minterContract = Scrollw3.eth.contract(address=minter_address, abi=minter_abi)
+
+activePeriod = minterContract.functions.active_period().call()
+activePeriodEnd = int(activePeriod) + WEEK
+print(f"activePeriod : {activePeriod}")
+print(f"activePeriodEnd : {activePeriodEnd}")
+
+# SQLite 데이터베이스에 연결
+conn = sqlite3.connect('contract_data.db')
+cursor = conn.cursor()
+
+def handle_epoch():
+    global activePeriodEnd, cursor, conn
+    new_db_name = f"contract_data_{activePeriodEnd}.db"
+    new_conn = sqlite3.connect(new_db_name)
+    new_cursor = new_conn.cursor()
+
+    print(f"New database created: {new_db_name}")
+
+    # 기존 연결을 닫고 새로운 연결을 할당
+    cursor.close()
+    conn.close()
+    cursor = new_cursor
+    conn = new_conn
+
+def handle_event(event):
+    sender = event['args']['sender']
+    amount0 = event['args']['amount0']
+    amount1 = event['args']['amount1']
+
+    # 테이블이 없으면 새로운 테이블 생성
+    cursor.execute(f'''
+        CREATE TABLE IF NOT EXISTS {sender} (
+            id INTEGER PRIMARY KEY,
+            sender TEXT,
+            amount0 REAL,
+            amount1 REAL,
+            totalFee0 REAL,
+            totalFee1 REAL
+        )
+    ''')
+    conn.commit()
+
+    # 데이터베이스에 저장
+    cursor.execute(f"INSERT INTO {sender} (sender, amount0, amount1) VALUES (?, ?, ?)", (sender, amount0, amount1))
+    conn.commit()
+
+    print(f"Received and saved data: sender={sender}, amount0={amount0}, amount1={amount1}")
+
+    # totalfee0 및 totalfee1 가져오기
+    cursor.execute(f"SELECT totalFee0, totalFee1 FROM {sender} WHERE sender = ?", (sender,))
+    result = cursor.fetchone()
+
+    if result:
+        totalFee0, totalFee1 = result
+    else:
+        totalFee0, totalFee1 = 0, 0
+
+    # amount0 및 amount1 더하기
+    totalFee0 += amount0
+    totalFee1 += amount1
+
+    # totalFee0 및 totalFee1 업데이트
+    cursor.execute(f"UPDATE {sender} SET totalFee0 = ?, totalFee1 = ? WHERE sender = ?", (totalFee0, totalFee1, sender))
+    conn.commit()
+
+    print(f"Updated totalFee0={totalFee0}, totalFee1={totalFee1}")
+
+contract = Scrollw3.eth.contract(address=pairFactory_address, abi=pair_abi)
+event_filter = contract.events.Fees.createFilter(fromBlock="latest")
+
+while True:
+    current_block_number = Scrollw3.eth.block_number
+    print(f"current block: {current_block_number}")
+
+    if current_block_number >= activePeriodEnd:
+        handle_epoch()
+        activePeriod = minterContract.functions.active_period().call()
+        activePeriodEnd = int(activePeriod) + WEEK
+        print(f"activePeriodEnd updated: {activePeriodEnd}")
+
+    for event in event_filter.get_new_entries():
+        handle_event(event)
